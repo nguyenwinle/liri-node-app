c93cf0047b50077dfc1ad4528bc2fd4dec0e7d7f
Nguyen Le <ucnguyenle@gmail.com>
2018-08-11 14:31:50 -0700

    add apy and secret keys

--- keys.js
+++ keys.js
@@ -1,13 +1,13 @@
 console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  consumer_key: 'dS3nsfE0RFjqyebxFu7J2uAVH',
+  consumer_secret: 'IdjBJY21wIL8AquVWBnUbumq07guFkIZC99fZg4Y8ffOOHTw4x',
+  access_token_key: '3501159792-YlZXQVvUkFUWqm6u6mZV3ftK54VGGQxEy3E87q7',
+  access_token_secret: 'DznVhXPRi2RDdEsh3wykEhMD7kNTnNexPU6yZRqZl9G47'
 };
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: 'd0a4ab291b1448c3bcfb8d375b14e52f',
+  secret: '140e6bb76b6c4c6e86950f007872ff8a'
 };
